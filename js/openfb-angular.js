b9bf8276e46376b2992366cdbf3d31a09c87de57
Amreesh Tyagi <tyagi.amreesh@gmail.com>
2015-01-11 21:04:03 +0530

    set runningInCordova=true for all devices
    Change for now. Testing purpose

--- js/openfb-angular.js
+++ js/openfb-angular.js
@@ -67,6 +67,8 @@ angular.module('openfb', [])
             loginProcessed = false;
 
             logout();
+			
+			runningInCordova=true;
 
             // Check if an explicit oauthRedirectURL has been provided in init(). If not, infer the appropriate value
             if (!oauthRedirectURL) {
