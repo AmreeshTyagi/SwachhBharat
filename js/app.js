9865b2a1a45c57632d3882d0e8d970adcb88e42b
Amreesh Tyagi <tyagi.amreesh@gmail.com>
2015-01-11 19:59:54 +0530

    Swachh Bharat App
    Angular Client code

---
+++ js/app.js
@@ -0,0 +1,162 @@
+angular.module('swachhbharat', ['ionic', 'openfb', 'swachhbharat.controllers','firebase'])
+.factory('tokenInjector', [function() {  
+    var tokenInjector = {
+        request: function(config) {
+            if (window.sessionStorage['auth_token']!=null || typeof window.sessionStorage['auth_token']!='undefined') {
+                config.headers['sb-auth-token'] = window.sessionStorage['auth_token'];
+                config.headers['sb-user-id'] = window.sessionStorage['user_id'];
+            }
+            return config;
+        }
+    };
+    return tokenInjector;
+}])
+.config(['$httpProvider', function($httpProvider) {  
+    $httpProvider.interceptors.push('tokenInjector');
+}])
+ .run(function ($rootScope, $state, $ionicPlatform, $window, OpenFB,$firebase,$http) {
+$rootScope.FbAppId='383399838503387';
+        OpenFB.init($rootScope.FbAppId);
+        $rootScope.ServiceUrl="https://server-batman-1.c9.io";
+        
+         var ref = new Firebase("https://swachhbharat.firebaseio.com/users");
+         
+         var fb=$firebase(ref);
+         
+       // fb.$set({ name: "Swach Bharat App" });
+     //   fb.$set({uid:"saisasasih"});
+//      $http.get($rootScope.ServiceUrl+"loginStatus")
+// 		 .success(function(data){
+// 		        fb.$push(data);
+// 		 });
+  
+    // fb.$push();
+        
+//         fb.on("value", function(snapshot) {
+//   console.log(snapshot.val());
+// }, function (errorObject) {
+//   console.log("The read failed: " + errorObject.code);
+// });
+
+        $ionicPlatform.ready(function () {
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+
+        $rootScope.$on('$stateChangeStart', function(event, toState) {
+            if (toState.name !== "login" && toState.name !== "logout" && !$window.sessionStorage['auth_token']) {
+        
+                $state.go('login');
+                event.preventDefault();
+            }
+        });
+
+        $rootScope.$on('OAuthException', function() {
+            $state.go('login');
+        });
+
+    })
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+
+            .state('app', {
+                url: "/app",
+                abstract: true,
+                templateUrl: "templates/tabs.html",
+                controller: "AppCtrl"
+            })
+
+            .state('login', {
+                url: "/login",
+                 templateUrl: "templates/login.html",
+                        controller: "LoginCtrl"
+                // views: {
+                //     'menuContent': {
+                       
+                //     }
+                //}
+            })
+
+            .state('app.logout', {
+                url: "/logout",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/logout.html",
+                        controller: "LogoutCtrl"
+                    }
+                }
+            })
+
+            .state('app.profile', {
+                url: "/profile",
+                views: {
+                    'tab-profile': {
+                        templateUrl: "templates/profile.html",
+                        controller: "ProfileCtrl"
+                    }
+                }
+            })
+
+            .state('app.share', {
+                url: "/share",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/share.html",
+                        controller: "ShareCtrl"
+                    }
+                }
+            })
+
+            .state('app.friends', {
+                url: "/person/:personId/friends",
+                views: {
+                    'tab-friends': {
+                        templateUrl: "templates/friend-list.html",
+                        controller: "FriendsCtrl"
+                    }
+                }
+            })
+            .state('app.mutualfriends', {
+                url: "/person/:personId/mutualfriends",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/mutual-friend-list.html",
+                        controller: "MutualFriendsCtrl"
+                    }
+                }
+            })
+            .state('app.person', {
+                url: "/person/:personId",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/person.html",
+                        controller: "PersonCtrl"
+                    }
+                }
+            })
+            .state('app.feed', {
+                url: "/person/:personId/feed",
+                views: {
+                    'tab-feeds': {
+                        templateUrl: "templates/feed.html",
+                        controller: "FeedCtrl"
+                    }
+                }
+            }).
+            state('app.nomination', {
+                url: "/person/:personId/nomination",
+                views: {
+                    'tab-nomination': {
+                        templateUrl: "templates/nomination.html",
+                        controller: "NominationCtrl"
+                    }
+                }
+            })
+            ;
+
+        // fallback route
+        $urlRouterProvider.otherwise('/app/person/me/feed');
+
+    });
+
---
+++ js/controllers.js
@@ -0,0 +1,191 @@
+angular.module('swachhbharat.controllers', [])
+
+.controller('AppCtrl', function($scope, $state, OpenFB) {
+
+    $scope.logout = function() {
+        OpenFB.logout();
+        window.sessionStorage['user_id']=undefined;
+        $state.go('login');
+    };
+
+    $scope.revokePermissions = function() {
+        OpenFB.revokePermissions().then(
+            function() {
+                $state.go('login');
+            },
+            function() {
+                alert('Revoke permissions failed');
+            });
+    };
+
+})
+
+.controller('LoginCtrl', function($scope, $rootScope, $http, $location, OpenFB) {
+    // var ref = new Firebase("https://swachhbharat.firebaseio.com");
+
+    //   var fb=$firebase(ref);
+    $scope.login = function(provider, username, password) {
+        switch (provider) {
+            case 'facebook':
+                $scope.facebookLogin();
+                break;
+
+            default:
+                // code
+        }
+    };
+
+    $scope.facebookLogin = function() {
+
+        OpenFB.login('email,read_stream,publish_stream').then(
+            function() {
+
+                OpenFB.get('/me').success(function(user) {
+                    $scope.postLoginData(user, 'fb');
+                });
+
+                //    fb.$push({ uid: "hi" });
+                // return window.sessionStorage['auth_token'];
+                // $location.path('/app/person/me/feed');
+            },
+            function() {
+                alert('OpenFB login failed');
+            });
+    };
+
+    $scope.postLoginData = function(user, provider) {
+        // window.sessionStorage.id=user.id;
+        // var auth_token={};
+        // data.id=user.id;
+        var auth_token = window.sessionStorage['auth_token'];
+        // data.auth_provider=provider;
+        $http.post($rootScope.ServiceUrl + "/login", {
+                "data": null,
+                "auth_token": auth_token,
+                "auth_provider": provider
+            })
+            .success(function(data) {
+                if (data.success) {
+                    window.sessionStorage['auth_token'] = data.auth_token;
+                    window.sessionStorage['user_id'] = data.user_id;
+                    $location.path('/app/person/me/feed');
+                }
+                // fb.$push(data);
+            });
+
+    };
+
+
+})
+
+.controller('ShareCtrl', function($scope, OpenFB) {
+
+    $scope.item = {};
+
+    $scope.share = function() {
+        OpenFB.post('/me/feed', $scope.item)
+            .success(function() {
+                $scope.status = "This item has been shared on OpenFB";
+            })
+            .error(function(data) {
+                alert(data.error.message);
+            });
+    };
+
+})
+
+.controller('ProfileCtrl', function($scope, $rootScope, $location, OpenFB, $http) {
+    $http.get($rootScope.ServiceUrl + "/profile")
+        .success(function(data) {
+            if (data.success) {
+                console.log(data);
+                $scope.profile = data.profile;
+                $location.path('/app/profile');
+            }
+            else {
+               $rootScope.$emit('OAuthException');
+            }
+            // fb.$push(data);
+        });
+})
+
+.controller('PersonCtrl', function($scope, $stateParams, OpenFB) {
+    OpenFB.get('/' + $stateParams.personId).success(function(user) {
+        $scope.user = user;
+    });
+})
+
+.controller('FriendsCtrl', function($scope, $stateParams, OpenFB) {
+    // console.log($stateParams);
+    OpenFB.get('/' + $stateParams.personId + '/friends', {
+            limit: 50
+        })
+        .success(function(result) {
+            console.log(result);
+            $scope.friends = result.data;
+            // console.log($scope.friends);
+        })
+        .error(function(data) {
+            alert(data.error.message);
+        });
+})
+
+.controller('MutualFriendsCtrl', function($scope, $stateParams, OpenFB) {
+    OpenFB.get('/' + $stateParams.personId + '/mutualfriends', {
+            limit: 50
+        })
+        .success(function(result) {
+            $scope.friends = result.data;
+        })
+        .error(function(data) {
+            alert(data.error.message);
+        });
+})
+
+.controller('FeedCtrl', function($scope, $stateParams, OpenFB, $ionicLoading) {
+
+        $scope.show = function() {
+            $scope.loading = $ionicLoading.show({
+                content: 'Loading...'
+            });
+        };
+        $scope.hide = function() {
+            $scope.loading.hide();
+        };
+
+        function loadFeed() {
+            $scope.show();
+            OpenFB.get('/' + $stateParams.personId + '/home', {
+                    limit: 30
+                })
+                .success(function(result) {
+                    $scope.hide();
+                    $scope.items = result.data;
+                    // Used with pull-to-refresh
+                    $scope.$broadcast('scroll.refreshComplete');
+                })
+                .error(function(data) {
+                    $scope.hide();
+                    alert('hi');
+                    alert(data.error.message);
+                });
+        }
+
+        $scope.doRefresh = loadFeed;
+
+        loadFeed();
+
+    })
+    .controller('NominationCtrl', function($scope, $stateParams, OpenFB) {
+        //  console.log($stateParams);
+        OpenFB.get('/' + $stateParams.personId + '/friends', {
+                limit: 50
+            })
+            .success(function(result) {
+                $scope.friends = result.data;
+                console.log($scope.friends);
+            })
+            .error(function(data) {
+                alert(data.error.message);
+            });
+    });
